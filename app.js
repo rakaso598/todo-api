28da92350f85e449ff652ea4b992e12a02c57748
rakaso598 <nexxus86@outlook.com>
2025-03-06 18:35:59 +0900

    Added: create task Api

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from "express";
 import tasks from "./data/mock.js";
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // middleware
+// express.json(): 헤더의 content-type이 application/json이라면 body를 파싱해주는 미들웨어.
+// req.body
 
-// 쿼리스트링
+// 쿼리스트링: req.query
 app.get("/tasks", (req, res) => {
   const sort = req.query.sort;
   const count = Number(req.query.count);
@@ -23,7 +25,7 @@ app.get("/tasks", (req, res) => {
   res.send(newTasks);
 });
 
-// 파라미터
+// 파라미터: req.params
 app.get("/tasks/:id", (req, res) => {
   const id = Number(req.params.id);
   const task = tasks.find((task) => task.id === id);
@@ -37,4 +39,18 @@ app.get("/tasks/:id", (req, res) => {
 
 app.listen(3001, () => {
   console.log("server stated!");
+});
+
+// POST: req.body
+app.post("/tasks", () => {
+  const newTask = req.body;
+
+  const ids = tasks.map((task) => task.id); // [1,2,3,4,5]
+  newTask.id = Math.max(...ids) + 1;
+  newTask.isComplete = false;
+  newTask.createdAt = new Date();
+  newTask.updatedAt = new Date();
+  tasks.push(newTask);
+
+  res.status(201).send(newTask); // 201 created
 });
