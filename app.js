a5a3c4871b9f2c717ab06e2f2b546894a9fc3f19
rakaso598 <nexxus86@outlook.com>
2025-03-06 17:45:42 +0900

    Added: get request Query

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import express from "express";
+import tasks from "./data/mock.js";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("app.get hello");
+app.get("/tasks", (req, res) => {
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  const compareFn =
+    sort === "oldest"
+      ? (a, b) => a.createdAt - b.createdAt
+      : (a, b) => b.createdAt - a.createdAt;
+
+  let newTasks = tasks.sort(compareFn);
+
+  if (count) {
+    newTasks = newTasks.slice(0, count);
+  }
+
+  res.send(newTasks);
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("server stated!");
 });
-// app.listen(숫자,함수)
