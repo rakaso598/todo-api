2e03683f2f6e32cd78289c6d9718f14f53bf107f
rakaso598 <nexxus86@outlook.com>
2025-03-06 16:52:25 +0900

    Added: request GET hello

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.send("app.get hello");
+});
+
 app.listen(3000, () => {
   console.log("server stated!");
 });
